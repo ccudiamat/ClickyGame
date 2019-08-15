41250bee570c375dcb5362b366b0b2a26d0ba014
Christopher Cudiamat <ccudiamat93@gmail.com>
2019-08-14 23:54:03 -0700

    updated App.js

--- src/App.js
+++ src/App.js
@@ -6,30 +6,42 @@ import Wrapper from "./components/Wrapper"
 
 class App extends React.Component {
     state = {
-      avatar,
-      score: 0,
-      highscore: 0
+        avatar,
+        score: 0,
+        highscore: 0
     };
-  
+
     clickedId = () => {
-        
+
+    }
+
+    gameOver = () => {
+        if (this.state.score > this.state.highscore) {
+            this.setState({ highscore: this.state.score });
+        }
+        this.state.avatar.forEach(character => {
+            character.clicked = 0;
+        });
+        alert(`Game Over :( \nscore: ${this.state.score}`);
+        this.setState({ score: 0 });
     }
 
     render() {
-      return (
-        <React.Fragment>
-          <Title title="Avatar: The Last Airbender Memory Game" score={this.state.score} highscore={this.state.highscore}/>
-          <Wrapper>
-          {this.state.avatar.map(character => (
-            <CardHolder
-              {...character}
-              />
-          ))}
-          </Wrapper>
-        </React.Fragment>
-      );
+        return (
+            <React.Fragment>
+                <Title title="Avatar: The Last Airbender Memory Game" score={this.state.score} highscore={this.state.highscore} />
+                <Wrapper>
+                    {this.state.avatar.map(character => (
+                        <CardHolder
+                            clickedId={this.clickedId}
+                            id={character.id}
+                            image={character.image}
+                        />
+                    ))}
+                </Wrapper>
+            </React.Fragment>
+        );
     }
-  }
-  
-  export default App;
-  
+}
+
+export default App;
