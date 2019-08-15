62e3ab86355fc6e515d5dc97ffa52ae18061332d
Christopher Cudiamat <ccudiamat93@gmail.com>
2019-08-14 22:53:35 -0700

    updated App.js file with Wrapper component and css

--- src/App.js
+++ src/App.js
@@ -2,21 +2,30 @@ import React from "react";
 import CardHolder from "./components/CardHolder";
 import Title from "./components/Title"
 import avatar from "./avatar.json";
+import Wrapper from "./components/Wrapper"
 
 class App extends React.Component {
     state = {
-      avatar
+      avatar,
+      score: 0,
+      highscore: 0
     };
   
+    clickedId = () => {
+        
+    }
+
     render() {
       return (
         <React.Fragment>
-          <Title title="Avatar: The Last Airbender Memory Game" />
+          <Title title="Avatar: The Last Airbender Memory Game" score={this.state.score} highscore={this.state.highscore}/>
+          <Wrapper>
           {this.state.avatar.map(character => (
             <CardHolder
               {...character}
               />
           ))}
+          </Wrapper>
         </React.Fragment>
       );
     }
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./style.css";
 
 function Title(props) {
     return (
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
                 <h1 className="display-4">{props.title}</h1>
+                <p className="lead"><strong>SCORE: </strong>{props.score} <strong>HIGH SCORE: </strong>{props.highscore}</p>
             </div>
         </div>
     );
