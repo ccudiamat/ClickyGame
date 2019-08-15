a01646e16944bb7ca6c988b9bbdb71ef0908e4b1
Christopher Cudiamat <ccudiamat93@gmail.com>
2019-08-14 22:22:02 -0700

    updated card holder and added maps and css

--- src/App.js
+++ src/App.js
@@ -1,2 +1,25 @@
 import React from "react";
 import CardHolder from "./components/CardHolder";
+import avatar from "./avatar.json";
+
+class App extends React.Component {
+    state = {
+      avatar
+    };
+  
+    render() {
+      return (
+        <React.Fragment>
+          <h1 className="title">Avatar: The Last Airbender Memory Game</h1>
+          {this.state.avatar.map(character => (
+            <CardHolder
+              {...character}
+              />
+          ))}
+        </React.Fragment>
+      );
+    }
+  }
+  
+  export default App;
+  
